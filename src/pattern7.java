4a4e7fcd6de535977841bf1ddcc856df5a94506a
Atul sharma <satul9900@gmail.com>
2023-07-24 21:11:39 +0530

    New starting

--- src/pattern7.java
+++ src/pattern7.java
@@ -1,12 +1,14 @@
-public class Ipattern {
+import java.util.Scanner;
+public class pattern7 {
     public static void main(String[] args)
     {
-        int n=5;
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
         for (int i=0; i<n; i++)
         {
             for (int j=0; j<n; j++)
             {
-                if ( i==0 || i==n-1 || j==(n-1)/2  )
+                if (i==j || i+j==n-1 || i==0 || i==n-1 || j==0 || j==n-1 )
                 {
                     System.out.print("*");
                 }else
@@ -20,5 +22,3 @@ public class Ipattern {
 
     }
 }
-
-
